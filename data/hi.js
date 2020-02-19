a5b3b1d11a3d72b88a3a790481811c9e051cb22a
Rajesh <rajesh.kumaravel@tarento.com>
2020-02-19 16:58:55 +0530

    Hindi and Kannada question data updated.

--- data/hi.js
+++ data/hi.js
@@ -1,87 +1,130 @@
 var hi_data = [
-    {
-        "questionNumber": 1,
-        "questionText": "वह बहुत बड़ा राजा जिसके अधीन अनेक राजा या राज्य हों",
-        "word":"शाहंशाह"
-    },
-    {
-        "questionNumber": 2,
-        "questionText": "वह समय जब दिन का अंत और रात का आरंभ होने को होता है",
-        "word":"सायंकाल"
-    },
-    {
-        "questionNumber": 3,
-        "questionText": "वह व्यक्ति जो अभी कुछ सीख रहा हो पर उसमें पूरी तरह से निपुण न हो",
-        "word":"नौसिखुआ"
-    },
-    {
-        "questionNumber": 4,
-        "questionText": "पानी में उगने वाला एक सुन्दर पुष्प",
-        "word":"कमल"
-    },
-    {
-        "questionNumber": 5,
-        "questionText": "पानी रखने का मिट्टी का एक छोटा बरतन",
-        "word":"मटका"
-    },
-    {
-        "questionNumber": 6,
-        "questionText": "बहुत से तारों का एक विस्तृत समूह जो आकाश में उत्तर-दक्षिण फैला है",
-        "word":"आकाशगंगा"
-    },
-    {
-        "questionNumber": 7,
-        "questionText": "दूसरे के यहाँ रसोई बनाने वाला व्यक्ति",
-        "word":"रसोइया"
-    },
-    {
-        "questionNumber": 8,
-        "questionText": "बेटे या बेटी का पोता",
-        "word":"परपोता"
-    },
-    {
-        "questionNumber": 9,
-        "questionText": "पुराणानुसार पृथ्वी के नीचे के सात लोकों में से सबसे नीचे का या सातवाँ लोक",
-        "word":"नागलोक"
-    },
-    {
-        "questionNumber": 10,
-        "questionText": "गाँव या देहातों में गाए जानेवाले जनसाधारण के वे गीत",
-        "word":"लोकगीत"
-    },
-    {
-        "questionNumber": 11,
-        "questionText": "किसी काम में साथ देने या सहायक होने की क्रिया",
-        "word":"योगदान"
-    },
-    {
-        "questionNumber": 12,
-        "questionText": "हमारा राष्ट्रीय खेल",
-        "word":"हॉकी"
-    },
-    {
-        "questionNumber": 13,
-        "questionText": "हमारा राष्ट्रगान किसने लिखा है?",
-        "word":"रबीन्द्रनाथठाकुर"
-    },
-    {
-        "questionNumber": 14,
-        "questionText": "वह व्यक्ति जो बर्तन बनाता है",
-        "word":"कुम्हार"
-    },
-    {
-        "questionNumber": 15,
-        "questionText": "यह हमारे देश का जानवर है",
-        "word":"बाघ"
-    },
-    {
-        "questionNumber": 16,
-        "questionText": "हमारे देश की राजधानी कौन सी है?",
-        "word":"दिल्ली"
-    },
-    {
-        "questionNumber": 17,
-        "questionText": "हमारे लिए सबसे करीबी तारा कौन सा है?",
-        "word":"सूरज"
-    }
+    [
+        {
+            "questionNumber": 1,
+            "questionText": "पानी रखने का मिट्टी का एक छोटा बरतन",
+            "word": "मटका"
+        },
+        {
+            "questionNumber": 2,
+            "questionText": "पानी में उगने वाला एक सुन्दर पुष्प",
+            "word": "कमल"
+        }, {
+            "questionNumber": 3,
+            "questionText": "दूसरे के यहाँ रसोई बनाने वाला व्यक्ति",
+            "word": "रसोइया"
+        }, {
+            "questionNumber": 4,
+            "questionText": "वह समय जब दिन का अंत और रात का आरंभ होने को होता है",
+            "word": "सायंकाल"
+        }, {
+            "questionNumber": 5,
+            "questionText": "वह व्यक्ति जो अभी कुछ सीख रहा हो पर उसमें पूरी तरह से निपुण न हो",
+            "word": "नौसिखुआ"
+        },
+    ],
+    [
+        {
+            "questionNumber": 1,
+            "questionText": "हमारा राष्ट्रीय खेल",
+            "word": "हॉकी"
+        },
+        {
+            "questionNumber": 2,
+            "questionText": "हमारा राष्ट्रगान किसने लिखा है?",
+            "word": "रबीन्द्रनाथठाकुर"
+        },
+        {
+            "questionNumber": 3,
+            "questionText": "किसी काम में साथ देने या सहायक होने की क्रिया",
+            "word": "योगदान"
+        },
+        {
+            "questionNumber": 4,
+            "questionText": "पुराणानुसार पृथ्वी के नीचे के सात लोकों में से सबसे नीचे का या सातवाँ लोक",
+            "word": "नागलोक"
+        },
+        {
+            "questionNumber": 5,
+            "questionText": "बहुत से तारों का एक विस्तृत समूह जो आकाश में उत्तर-दक्षिण फैला है",
+            "word": "आकाशगंगा"
+        },
+    ],
+    [
+        {
+            "questionNumber": 1,
+            "questionText": "यह हमारे देश का जानवर है",
+            "word": "बाघ"
+        },
+        {
+            "questionNumber": 2,
+            "questionText": "बेटे या बेटी का पोता",
+            "word": "परपोता"
+        },
+        {
+            "questionNumber": 3,
+            "questionText": "वह व्यक्ति जो बर्तन बनाता है",
+            "word": "कुम्हार"
+        },
+        {
+            "questionNumber": 4,
+            "questionText": "वह बहुत बड़ा राजा जिसके अधीन अनेक राजा या राज्य हों",
+            "word": "शाहंशाह"
+        },
+        {
+            "questionNumber": 5,
+            "questionText": "गाँव या देहातों में गाए जानेवाले जनसाधारण के वे गीत",
+            "word": "लोकगीत"
+        },
+    ],
+    [
+        {
+            "questionNumber": 1,
+            "questionText": "हमारे देश की राजधानी कौन सी है?",
+            "word": "दिल्ली"
+        },
+        {
+            "questionNumber": 2,
+            "questionText": "हमारे लिए सबसे करीबी तारा कौन सा है?",
+            "word": "सूरज"
+        },
+        {
+            "questionNumber": 3,
+            "questionText": "देश में राष्ट्रीय न्यादर्श (N.S.S.)की स्थापना कबहुई?",
+            "word": "1927"
+        },
+        {
+            "questionNumber": 4,
+            "questionText": "भारत को अधिकतम किससे कर आय प्राप्त होती है?",
+            "word": "बिजली कर"
+        },
+        {
+            "questionNumber": 5,
+            "questionText": "विश्व में सेलफोन का सबसे बड़ा उत्पादक देश कौनसा है?",
+            "word": "भारत"
+        }
+    ],
+    [
+        {
+            "questionNumber": 1,
+            "questionText": "",
+            "word": "भारत"
+        }, {
+            "questionNumber": 2,
+            "questionText": "विश्व में सबसे अधिक कोयला उत्पादन करने वाला देश हैं?",
+            "word": "भारत"
+        }, {
+            "questionNumber": 3,
+            "questionText": "हिन्द महासागर और लाल सागर को कौनसी जलसन्धि जोड़ती हैं?",
+            "word": "बाब-अल-मन्देब"
+        }, {
+            "questionNumber": 4,
+            "questionText": "अरब सागर के पानी का औसतन खारापन हैं?",
+            "word": "45ppt"
+        }, {
+            "questionNumber": 5,
+            "questionText": "एक ही प्रकार का परमाणु निम्नलिखित में से किसमें मिलता हैं?",
+            "word": "प्राकृत तत्व"
+        },
+    ]
 ]
