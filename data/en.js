cadeb1be53021e8959b97d0062dd02f7d7225629
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-02-19 14:57:25 +0530

    question set changes

--- data/en.js
+++ data/en.js
@@ -1,102 +1,127 @@
 var en_data = [
-    {
+    [{
         "questionNumber": 1,
-        "questionText": "A person who thinks only of himself",
-        "word":"egoist"
+        "questionText": "what direction does the sun rise",
+        "word": "east"
     },
     {
         "questionNumber": 2,
-        "questionText": "A life history written by somebody else",
-        "word":"biography"
+        "questionText": "A person who thinks only of himself",
+        "word": "egoist"
     },
     {
         "questionNumber": 3,
-        "questionText": "The act of murder of a human being.",
-        "word":"homicide"
+        "questionText": "A life history written by somebody else",
+        "word": "biography"
     },
     {
         "questionNumber": 4,
-        "questionText": "Gayatri doesn't know how to read and write .Her friends call her",
-        "word":"illiterate"
+        "questionText": "One who can speak two languages",
+        "word": "bilingual"
     },
     {
         "questionNumber": 5,
-        "questionText": "A thing no longer in use",
-        "word":"obsolete"
-    },
-    {
-        "questionNumber": 6,
-        "questionText": "People who work together",
-        "word":"colleagues"
-    },
-    {
-        "questionNumber": 7,
-        "questionText": "One who goes on foot",
-        "word":"pedestrian"
-    },
-    {
-        "questionNumber": 8,
-        "questionText": "One who can speak two languages",
-        "word":"bilingual"
-    },
-    {
-        "questionNumber": 9,
-        "questionText": "Word with the same meaning",
-        "word":"synonyms"
-    },
-    {
-        "questionNumber": 10,
-        "questionText": "The person who works for free",
-        "word":"volunteer"
-    },
-    {
-        "questionNumber": 11,
-        "questionText": "Life history written by oneself",
-        "word":"autobiography"
-    },
-    {
-        "questionNumber": 12,
         "questionText": "The person who collects coins",
-        "word":"numismatist"
-    },
-    {
-        "questionNumber": 13,
-        "questionText": "One who does not believes in god",
-        "word":"atheist"
-    },
-    {
-        "questionNumber": 14,
-        "questionText": "Person who writes with both hands",
-        "word":"ambidextrous"
-    },
-    {
-        "questionNumber": 15,
-        "questionText": "A thing that is kept as a reminder of an event",
-        "word":"souvenir"
-    },
-    {
-        "questionNumber": 16,
-        "questionText": "One who goes on a journey to a holy place",
-        "word":"pilgrim"
-    },
-    {
-        "questionNumber": 17,
-        "questionText": "Thing which is not fresh?",
-        "word":"stale"
-    },
-    {
-        "questionNumber": 18,
-        "questionText": "Lesson to be learned from a story",
-        "word":"moral"
-    },
-    {
-        "questionNumber": 19,
-        "questionText": "Handwritten document",
-        "word":"manuscript"
-    },
-    {
-        "questionNumber": 20,
-        "questionText": "One who is present everywhere",
-        "word":"omnipresent"
+        "word": "numismatist"
     }
-]
+    ], [{
+        "questionNumber": 1,
+        "questionText": "what is the color of danger",
+        "word": "red"
+    },
+    {
+        "questionNumber": 2,
+        "questionText": "Gayatri doesn't know how to read and write .Her friends call her",
+        "word": "illiterate"
+    },
+    {
+        "questionNumber": 3,
+        "questionText": "People who work together",
+        "word": "colleagues"
+    },
+    {
+        "questionNumber": 4,
+        "questionText": "Word with the same meaning",
+        "word": "synonyms"
+    },
+    {
+        "questionNumber": 5,
+        "questionText": "Life history written by oneself",
+        "word": "autobiography"
+    }
+    ], [{
+        "questionNumber": 1,
+        "questionText": "orange is a color and also a",
+        "word": "fruit"
+    },
+    {
+        "questionNumber": 2,
+        "questionText": "The act of murder of a human being.",
+        "word": "homicide"
+    },
+    {
+        "questionNumber": 3,
+        "questionText": "One who goes on foot",
+        "word": "pedestrian"
+    },
+    {
+        "questionNumber": 4,
+        "questionText": "The person who works for free",
+        "word": "volunteer"
+    },
+    {
+        "questionNumber": 5,
+        "questionText": "Person who writes with both hands",
+        "word": "ambidextrous"
+    }
+    ], [{
+        "questionNumber": 1,
+        "questionText": "The profession of educating people",
+        "word": "teaching"
+    },
+    {
+        "questionNumber": 2,
+        "questionText": "A thing no longer in use",
+        "word": "obsolete"
+    },
+    {
+        "questionNumber": 3,
+        "questionText": "One who does not believes in god",
+        "word": "atheist"
+    },
+    {
+        "questionNumber": 4,
+        "questionText": "A thing that is kept as a reminder of an event",
+        "word": "souvenir"
+    },
+    {
+        "questionNumber": 5,
+        "questionText": "Thing which is not fresh?",
+        "word": "stale"
+    }
+    ], [{
+        "questionNumber": 1,
+        "questionText": "item used to grooming the hair",
+        "word": "comb"
+    },
+    {
+        "questionNumber": 2,
+        "questionText": "One who goes on a journey to a holy place",
+        "word": "pilgrim"
+    },
+    {
+        "questionNumber": 3,
+        "questionText": "Lesson to be learned from a story",
+        "word": "moral"
+    },
+    {
+        "questionNumber": 4,
+        "questionText": "Handwritten document",
+        "word": "manuscript"
+    },
+    {
+        "questionNumber": 5,
+        "questionText": "One who is present everywhere",
+        "word": "omnipresent"
+    }
+    ]]
